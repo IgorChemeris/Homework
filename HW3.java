8433f603dabb1d7bf4c24c1f6e9aa740d580091a
Igor <Igor.Chemeris@kyivstar.net>
2023-06-08 23:02:07 +0300

    Commit3

---
+++ HW3.java
@@ -0,0 +1,11 @@
+import java.util.List;
+public class HW3 {
+    public static void main(String[] args) {
+        String country = "Ukraine";
+        System.out.println(country.toUpperCase());
+        System.out.println(country.toLowerCase());
+        System.out.println(country.substring(0,1));
+        System.out.println(country.substring(country.length()-1, country.length()));
+
+    }
+}
