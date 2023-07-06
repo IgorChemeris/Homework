006072bc2bb9c59e818e97c0cf96a556bf635b0f
IgorChemeris <Igor.Chemeris@kyivstar.net>
2023-07-06 23:17:05 +0300

    Commit9

---
+++ hw9/HW9.java
@@ -0,0 +1,14 @@
+package hw9;
+
+public class HW9 {
+    public static void main(String[] args) {
+
+        Automobile car = new Car();
+        Automobile truck = new Truck();
+
+        car.automobileType();
+        truck.automobileType();
+
+    }
+
+}
